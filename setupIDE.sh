a55426ba7ec12aa172e473edff632ddf5bffb74f
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-06-07 15:31:00 +0900

    Fix minor issue

--- setupIDE.sh
+++ setupIDE.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+WD=$(pwd)
+
 sudo apt update
 sudo apt install cscope ctags
 
 mkdir -p ~/bin
-ln -s mktags ~/bin/
-ln -s .vimrc ~/.vimrc
+rm -f ~/bin/mktags
+ln -s ${WD}/mktags ~/bin/
+rm -f ~/.vimrc
+ln -s ${WD}/.vimrc ~/.vimrc
 
-if [[ ! -d ~/.vim/bundle/Vundle ]]
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]
 then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 fi
