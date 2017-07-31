4a1c8a8dff0a02236f7d6c3e3592042cae0afa40
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-08-01 04:05:44 +0900

    Update pyenv install script

--- install_scripts/python.sh
+++ install_scripts/python.sh
@@ -5,43 +5,38 @@ PYTHONE_DONE=
 set -e
 
 if [ ! -d configurations ];then
-	ROOT=$(pwd)/..
+    ROOT=$(pwd)/..
 else
-	ROOT=$(pwd)
+    ROOT=$(pwd)
 fi
 
-if [ ! $CONFIGURATIONS_DONE ];then
-    $($ROOT/install_scripts/configurations.sh)
+if [ ! $DEPENDENCIES_DONE ];then
+    source "$ROOT/install_scripts/dependencies.sh"
 fi
 
-echo "Git installation.. pwd: $PWD, root: $ROOT"
-
-PWD=$(pwd)
+echo "Pyenv installation.. pwd: $PWD, root: $ROOT"
 
 mkdir -p $HOME/.lib
 
-VERSION=3.6.2
-cd $HOME/.lib
-if [ ! -f Python-$VERSION.tgz ];then
-    wget https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
-    tar -xvf Python-$VERSION.tgz
+if ! grep -q "PYENV_ROOT" "$ENVFILE" ;then
+    echo '## PYENV CONFIGURATION ##' >> $ENVFILE
+    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $ENVFILE
+    echo 'export PATH=$PYENV_ROOT/bin:$PATH' >> $ENVFILE
+    echo 'eval "$(pyenv init -)"' >> $ENVFILE
+    echo 'eval "$(pyenv virtualenv-init -)"' >> $ENVFILE
 fi
-cd Python-$VERSION
-./configure --prefix=$HOME/bin
-make -j$CORE
-make test
-make install
 
-VERSION=2.7.13
-cd $HOME/.lib
-if [ ! -f Python-$VERSION.tgz ];then
-    wget https://www.python.org/ftp/python/$VERSION/Python-$VERSION.tgz
-    tar -xvf Python-$VERSION.tgz
-fi
-cd Python-$VERSION
-./configure --prefix=$HOME/bin
-make -j$CORE
-make test
-make altinstall
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH=$PYENV_ROOT/bin:$PATH
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+pyenv install 2.7.13
+pyenv install 3.6.1
+
+echo "---"
+echo "Type \"source $ENVFILE\""
 
 PYTHONE_DONE=1
