9c1dc44f3681443e5b8ec0bbf987dc079be7f784
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-09-12 21:31:25 +0900

    Add rm option to docker test

--- install_scripts/docker.sh
+++ install_scripts/docker.sh
@@ -14,27 +14,27 @@ fi
 
 echo "Docker Installation.. pwd: $PWD, root: $ROOT, core: $CORE"
 if [ $OS == "ubuntu" ];then
-    ${SUDO} apt-get remove docker docker-engine docker.io
-    ${SUDO} apt-get install \
+    ${SUDO} apt-get remove -y docker docker-engine docker.io
+    ${SUDO} apt-get install -y \
         apt-transport-https \
         ca-certificates \
         curl \
         software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | ${SUDO} apt-key add -
-    ${SUDO} add-apt-repository \
+    ${SUDO} add-apt-repository -y \
        "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
        $(lsb_release -cs) \
        stable"
-    ${SUDO} apt-get update
-    ${SUDO} apt install docker-ce
+    ${SUDO} apt-get update -y
+    ${SUDO} apt install -y docker-ce
 elif [ $OS == "cent" ];then
-    ${SUDO} yum remove docker docker-common docker-selinux docker-engine
-    ${SUDO} yum install yum-utils device-mapper-persistent-data lvm2
-    ${SUDO} yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    ${SUDO} yum remove -y docker docker-common docker-selinux docker-engine
+    ${SUDO} yum install -y yum-utils device-mapper-persistent-data lvm2
+    ${SUDO} yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
     ${SUDO} yum makecache fast
-    ${SUDO} yum install docker-ce
+    ${SUDO} yum install -y docker-ce
     ${SUDO} systemctl start docker
-    ${SUDO} docker run hello-world
+    ${SUDO} docker run --rm hello-world
 elif [ $OS == "mac" ];then
     curl -L https://download.docker.com/mac/stable/Docker.dmg -o $HOME/Docker.dmg
     echo "Run $HOME/Docker.dmg manually"
