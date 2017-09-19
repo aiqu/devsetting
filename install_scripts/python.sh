26cfc80d69f21cc1598601dc456eaada2b8b24a2
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-09-19 21:39:50 +0900

    update python install script

--- install_scripts/python.sh
+++ install_scripts/python.sh
@@ -20,18 +20,17 @@ mkdir -p $HOME/.lib
 
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH=$PYENV_ROOT/bin:$PATH
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
 
 curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -s 2.7.13
 env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install -s 3.6.1
 
-source $ENVFILE
-
-pip install jupyter jupyterthemes
-jt -t grade3 -f source -fs 95 -altp -tfs 11 -nfs 115 -cellw 88% -T
+$HOME/.pyenv/versions/2.7.13/bin/pip install jupyter jupyterthemes
+$HOME/.pyenv/versions/2.7.13/bin/jt -t grade3 -f source -fs 95 -altp -tfs 11 -nfs 115 -cellw 88% -T
 
 echo "---"
 echo "Type \"source $ENVFILE\""
