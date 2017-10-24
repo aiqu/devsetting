cc6e522a88c2e54cc75cc02b9f0d3a8cdec2ffd9
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-10-25 01:28:21 +0900

    changed flann foldername

--- install_scripts/flann.sh
+++ install_scripts/flann.sh
@@ -17,11 +17,12 @@ WORKDIR=$HOME/.lib
 
 cd $WORKDIR
 REPO_URL=https://github.com/mariusmuja/flann
-#TAG=$(git ls-remote --tags $REPO_URL | awk -F/ '{print $3}' | grep -v '{}' | grep -v '-' | sort -V | tail -n1)
-TAG='master'
-curl -LO $REPO_URL/archive/${TAG}.zip
-unzip ${TAG}.zip && rm -rf ${TAG}.zip flann
-mv flann-${TAG} flann
-cd flann && mkdir -p build && cd build
+TAG=$(git ls-remote --tags $REPO_URL | awk -F/ '{print $3}' | grep -v '{}' | grep -v '-' | sort -V | tail -n1)
+FOLDER="flann-${TAG}"
+if [ ! -d $FOLDER ]; then 
+  curl -LO $REPO_URL/archive/${TAG}.zip
+  unzip -q ${TAG}.zip && rm -rf ${TAG}.zip
+fi
+cd $FOLDER && mkdir -p build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$HOME/.local ..
 make -j$(nproc) && make install
