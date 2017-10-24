a20e64d314d83d860dea7a8e2565ddd186865f7d
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-10-25 01:18:45 +0900

    Fix eigen version to 3.3

--- install_scripts/eigen3.sh
+++ install_scripts/eigen3.sh
@@ -14,6 +14,7 @@ source $ROOT/envset.sh
 
 PWD=$(pwd)
 WORKDIR=$HOME/.lib
+VER='3.3'
 
 if [ $OS == 'centos' ]; then
   $SUDO yum install -y hg
@@ -25,6 +26,6 @@ cd $WORKDIR
 if [ ! -d eigen ]; then
   hg clone https://bitbucket.org/eigen/eigen/
 fi
-cd eigen && mkdir -p build && cd build && \
+cd eigen && hg up $VER &&  mkdir -p build && cd build && \
 cmake -DCMAKE_INSTALL_PREFIX=$HOME/.local .. && \
 make install
