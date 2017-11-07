59fdce144622a778d7bcdbfa0011b53fcc255517
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-07 23:30:17 +0900

    Add centos confirmation logic
    If there is no /etc/os-release file, check centos by existance of yum

--- envset.sh
+++ envset.sh
@@ -21,6 +21,8 @@ if [ $(echo $OSTYPE | grep 'linux') ];then
     ENVFILE="$HOME/.bashrc"
     if [[ -f /etc/os-release ]]; then
         OS=$(cat /etc/os-release | grep ^ID= | sed 's/ID=[^a-zA-Z]*\([a-zA-Z]\+\)[^a-zA-Z]*/\1/')
+    elif [ $(which yum 2>/dev/null) ]; then
+        OS='centos'
     else
         echo "Unknown linux distro"
         exit 1
