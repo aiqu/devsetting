83851191290c2bfb6f3c657ae68cbd596d822b73
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-07 23:33:30 +0900

    Add tmux install script

---
+++ install_scripts/tmux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+
+bash $ROOT/install_scripts/libevent.sh
+bash $ROOT/install_scripts/ncurses.sh
+
+TMP_DIR=$ROOT/tmp
+REPO_URL=https://github.com/tmux/tmux
+TAG=$(git ls-remote --tags $REPO_URL | awk -F/ '{print $3}' | grep -v '{}' | sort -V | tail -n1)
+FOLDER="tmux-$(echo $TAG | sed 's/v//')"
+INSTALLED_VERSION=$(tmux -V | cut -d' ' -f2)
+
+if [ -z $INSTALLED_VERSION ] || [ $TAG != $INSTALLED_VERSION ]; then
+  echo "tmux $TAG installation.. pwd: $PWD, root: $ROOT, core: $CORE"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+
+  curl -LO ${REPO_URL}/archive/${TAG}.zip
+  unzip -q ${TAG}.zip
+  cd $FOLDER
+  echo $(pwd)
+  ./autogen.sh && ./configure --prefix=$HOME/.local
+  make -j$(nproc) && make install
+
+  cd $PWD
+  rm -rf $TMP_DIR
+else
+  echo "tmux $INSTALLED_VERSION is already installed"
+fi
+
+cd $ROOT
