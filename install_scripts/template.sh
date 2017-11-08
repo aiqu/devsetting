676281d55510dd5346aa50f6894d2550bb38f8d3
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-08 17:17:39 +0900

    Add basic install script template

---
+++ install_scripts/template.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+
+PKG_NAME=""
+TMP_DIR=$ROOT/tmp
+REPO_URL=""
+TAG=$(git ls-remote -t $REPO_URL | grep -v {} | cut -d/ -f3 | sort -V | tail -n1)
+VER=""
+FOLDER="$PKG_NAME*"
+VERFILE=""
+INSTALLED_VERSION=""
+
+if [ -z $INSTALLED_VERSION ] || [ $TAG != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $TAG installation.. pwd: $PWD, root: $ROOT, core: $CORE"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -LO $REPO_URL/archive/$TAG.zip
+  unzip -q $TAG.zip && rm -rf $TAG.zip && cd $FOLDER
+  curl -L $REPO_URL | tar xz && cd $FOLDER
+  ./configure --prefix=$HOME/.local && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $INSTALLED_VERSION is already installed"
+fi
+
+cd $ROOT
