7f0616ecdf310894dfaafd0c510858806b250b28
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-09 17:15:23 +0900

    Update template

--- install_scripts/template.sh
+++ install_scripts/template.sh
@@ -4,29 +4,32 @@ set -e
 
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 PWD=$(pwd)
+. $ROOT/envset.sh
 
 PKG_NAME=""
 TMP_DIR=$ROOT/tmp
 REPO_URL=""
 TAG=$(git ls-remote -t $REPO_URL | grep -v {} | cut -d/ -f3 | sort -V | tail -n1)
-VER=""
+VER=$TAG
 FOLDER="$PKG_NAME*"
 VERFILE=""
 INSTALLED_VERSION=""
 
-if [ -z $INSTALLED_VERSION ] || [ $TAG != $INSTALLED_VERSION ]; then
-  echo "$PKG_NAME $TAG installation.. pwd: $PWD, root: $ROOT, core: $CORE"
+if [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT, core: $CORE"
 
   mkdir -p $TMP_DIR && cd $TMP_DIR
   curl -LO $REPO_URL/archive/$TAG.zip
   unzip -q $TAG.zip && rm -rf $TAG.zip && cd $FOLDER
   curl -L $REPO_URL | tar xz && cd $FOLDER
+  mkdir -p build && cd build && \
+    cmake -DCMAKE_INSTALL_PREFIX=$HOME/.local .. && \
   ./configure --prefix=$HOME/.local && \
     make -j$(nproc) && make install
 
   cd $ROOT && rm -rf $TMP_DIR
 else
-  echo "$PKG_NAME $INSTALLED_VERSION is already installed"
+  echo "$PKG_NAME $VER is already installed"
 fi
 
 cd $ROOT
