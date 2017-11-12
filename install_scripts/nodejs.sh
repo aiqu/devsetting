5f4b604c1f6d752b20801ef8124fd75eba8f9feb
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-12 23:03:51 +0900

    Add nodejs install script

---
+++ install_scripts/nodejs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="node"
+TMP_DIR=$ROOT/tmp
+REPO_URL="https://github.com/nodejs/node"
+TAG=$(git ls-remote -t $REPO_URL | grep -v {} | cut -d/ -f3 | sort -V | tail -n1)
+VER=$(echo $TAG | sed 's/v//')
+FOLDER="$PKG_NAME*"
+VERFILE=""
+INSTALLED_VERSION=""
+
+if [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -LO $REPO_URL/archive/$TAG.zip
+  unzip -q $TAG.zip && rm -rf $TAG.zip && cd $FOLDER
+  ./configure --prefix=$HOME/.local && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
