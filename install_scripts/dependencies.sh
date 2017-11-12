7f5dae83e9df8b1409deb9261e2207d523de2a77
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-12 22:33:47 +0900

    Rearrange dependency install order & Add which install script

--- install_scripts/dependencies.sh
+++ install_scripts/dependencies.sh
@@ -21,27 +21,28 @@ if [ $OS == "mac" ]; then
     fi
 fi
 
-$ROOT/install_scripts/readline.sh
-$ROOT/install_scripts/cmake.sh
-$ROOT/install_scripts/ncurses.sh
-$ROOT/install_scripts/autoconf.sh
-$ROOT/install_scripts/texinfo.sh
-$ROOT/install_scripts/cscope.sh
-$ROOT/install_scripts/ctags.sh
-$ROOT/install_scripts/unzip.sh
-$ROOT/install_scripts/pkg-config.sh
-$ROOT/install_scripts/automake.sh
-$ROOT/install_scripts/gettext.sh
-$ROOT/install_scripts/libtool.sh
-$ROOT/install_scripts/libconfuse.sh
-$ROOT/install_scripts/openssl.sh
-$ROOT/install_scripts/curl.sh
-$ROOT/install_scripts/libexpat.sh
-$ROOT/install_scripts/zlib.sh
-$ROOT/install_scripts/bzip2.sh
-$ROOT/install_scripts/sqlite3sh
-$ROOT/install_scripts/perl-extutiles-makemaker.sh
+. $ROOT/install_scripts/libtool.sh
+. $ROOT/install_scripts/automake.sh
+. $ROOT/install_scripts/autoconf.sh
+. $ROOT/install_scripts/which.sh
+. $ROOT/install_scripts/pkg-config.sh
+. $ROOT/install_scripts/ncurses.sh
+. $ROOT/install_scripts/unzip.sh
+. $ROOT/install_scripts/gettext.sh
+. $ROOT/install_scripts/readline.sh
+. $ROOT/install_scripts/openssl.sh
+. $ROOT/install_scripts/zlib.sh
+. $ROOT/install_scripts/curl.sh
+. $ROOT/install_scripts/bzip2.sh
+. $ROOT/install_scripts/cmake.sh
+. $ROOT/install_scripts/texinfo.sh
+. $ROOT/install_scripts/cscope.sh
+. $ROOT/install_scripts/ctags.sh
+. $ROOT/install_scripts/libconfuse.sh
+. $ROOT/install_scripts/libexpat.sh
+. $ROOT/install_scripts/sqlite.sh
+. $ROOT/install_scripts/perl-extutiles-makemaker.sh
 
-$ROOT/install_scripts/byobu.sh
+. $ROOT/install_scripts/byobu.sh
 
 DEPENDENCIES_DONE=1
---
+++ install_scripts/which.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="which"
+TMP_DIR=$ROOT/tmp
+REPO_URL="http://ftp.gnu.org/gnu/which/which-2.21.tar.gz"
+FOLDER="$PKG_NAME*"
+
+if [ ! -f $HOME/.local/bin/which ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -L $REPO_URL | tar xz && cd $FOLDER
+  autoconf
+  ./configure --prefix=$HOME/.local && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
