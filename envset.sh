db95e6bb427f05c9c35df4800527a910819a5543
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-21 23:46:06 +0900

    Remove all.sh, nosudo.sh & add base.sh

--- envset.sh
+++ envset.sh
@@ -2,43 +2,40 @@
 
 set -e 
 
-if [ ! $__ENVSET ];then
-  if [ ! $ROOT ];then
-      if [ ! -d 'configurations' ];then
-          ROOT=$(pwd)/..
-      else
-          ROOT=$(pwd)
-      fi
-  fi
+if [ ! $ROOT ];then
+    if [ ! -d 'configurations' ];then
+        ROOT=$(pwd)/..
+    else
+        ROOT=$(pwd)
+    fi
+fi
 
-  PWD=$(pwd)
-  if [ $(whoami) != root ];then
-      SUDO="sudo"
-  else
-      SUDO=""
-  fi
+PWD=$(pwd)
+if [ $(whoami) != root ];then
+    SUDO="sudo"
+else
+    SUDO=""
+fi
 
-  if [ $(echo $OSTYPE | grep 'linux') ];then
-      ENVFILE="$HOME/.bashrc"
-      if [[ -f /etc/os-release ]]; then
-          OS=$(cat /etc/os-release | grep ^ID= | sed 's/ID=[^a-zA-Z]*\([a-zA-Z]\+\)[^a-zA-Z]*/\1/')
-      elif [ $(which yum 2>/dev/null) ]; then
-          OS='centos'
-      else
-          echo "Unknown linux distro"
-          exit 1
-      fi
-  elif [ $(echo $OSTYPE | grep 'darwin') ];then
-      ENVFILE="$HOME/.bash_profile"
-      OS="mac"
-  else
-      echo "Unkown distro"
-      exit 1
-  fi
+if [ $(echo $OSTYPE | grep 'linux') ];then
+    ENVFILE="$HOME/.bashrc"
+    if [[ -f /etc/os-release ]]; then
+        OS=$(cat /etc/os-release | grep ^ID= | sed 's/ID=[^a-zA-Z]*\([a-zA-Z]\+\)[^a-zA-Z]*/\1/')
+    elif [ $(which yum 2>/dev/null) ]; then
+        OS='centos'
+    else
+        echo "Unknown linux distro"
+        exit 1
+    fi
+elif [ $(echo $OSTYPE | grep 'darwin') ];then
+    ENVFILE="$HOME/.bash_profile"
+    OS="mac"
+else
+    echo "Unkown distro"
+    exit 1
+fi
 
-  echo "Current OS is "$OS
-  if [ -f $ENVFILE ];then
-    . $ENVFILE
-  fi
-  __ENVSET='done'
+echo "Current OS is "$OS
+if [ -f $ENVFILE ];then
+  . $ENVFILE
 fi
