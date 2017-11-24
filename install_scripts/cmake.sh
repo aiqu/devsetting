db9731ceb3823b86a9fda1a03aac66c6b6ef0bdb
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-24 20:03:15 +0900

    Add system curl option

--- install_scripts/cmake.sh
+++ install_scripts/cmake.sh
@@ -23,7 +23,7 @@ if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VER ] || [ $INSTALLED_VER != $VER ];th
   curl -LO ${REPO_URL}/archive/${TAG}.zip
   unzip -q ${TAG}.zip
   cd $FOLDER
-  ./bootstrap --prefix=$HOME/.local --parallel=$(nproc) --no-system-libs --system-libarchive --system-expat
+  ./bootstrap --prefix=$HOME/.local --parallel=$(nproc) --no-system-libs --system-libarchive --system-expat --system-curl
 
   make -j$(nproc) && make install
 
