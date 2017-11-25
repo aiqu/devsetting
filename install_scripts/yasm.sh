dcdaae2061bb12316d982a86425c3087371f3130
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-25 23:14:06 +0900

    Add yasm install script

---
+++ install_scripts/yasm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+#    yasm installer
+#
+#    Copyright (C) 2017 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="yasm"
+TMP_DIR=$ROOT/tmp
+TAG='1.3.0'
+VER=$TAG
+REPO_URL="http://www.tortall.net/projects/yasm/releases/yasm-$TAG.tar.gz"
+FOLDER="$PKG_NAME*"
+VERFILE=""
+INSTALLED_VERSION=$(yasm --version | head -n1 | cut -d' ' -f2)
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -L $REPO_URL | tar xz && cd $FOLDER
+  ./configure --prefix=$HOME/.local && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
