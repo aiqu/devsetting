370349a2b57a5c873a4626b0812cfa2e4d6ab2d4
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-25 23:21:06 +0900

    Add freetype2 install script

---
+++ install_scripts/freetype2.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#    freetype2 installer
+#
+#    Copyright (C) 2017 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="freetype2"
+TMP_DIR=$ROOT/tmp
+REPO_URL="https://git.savannah.gnu.org/git/freetype/freetype2.git"
+TAG=$(git ls-remote -t $REPO_URL | grep -v -e '{}\|freetype\|start\|import\|BETA\|REAL\|beta' | cut -d/ -f3 | sort -V | tail -n1)
+VER='21.0.15'
+DOWN_URL="https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-$TAG.tar.gz"
+FOLDER="$PKG_NAME*"
+VERFILE=""
+if $(pkg-config --exists $PKG_NAME);then
+  INSTALLED_VERSION=$(pkg-config --modversion $PKG_NAME)
+fi
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -L $DOWN_URL | tar xz && cd $FOLDER
+  ./autogen.sh && \
+    ./configure --prefix=$HOME/.local --with-sysroot=$HOME/.local/local && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
