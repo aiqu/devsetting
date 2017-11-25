e34b5503c303c2c86e2620f1c9605df2275c9c1b
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-25 23:50:01 +0900

    Fix typo on jdk8

--- install_scripts/jdk8.sh
+++ install_scripts/jdk8.sh
@@ -32,6 +32,6 @@ if ! java -version 2>&1 | grep -q '1.8' || ! javac -version 2>&1 | grep -q '1.8'
   wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/$FILENAME"
   tar xf $FILENAME && rm $FILENAME
   ln -s $HOME/.local/$FOLDERNAME/bin/* $HOME/.local/bin/
-else;
+else
   echo "JDK8 is already installed"
 fi
