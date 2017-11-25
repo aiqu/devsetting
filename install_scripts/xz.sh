15b1a3954e77766f02dfeb7a218bd837c7fa4cc3
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-25 21:52:16 +0900

    Add xz install script

---
+++ install_scripts/xz.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#    xz installer
+#
+#    Copyright (C) 2017 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="xz"
+TMP_DIR=$ROOT/tmp
+REPO_URL="https://git.tukaani.org/xz.git"
+TAG=$(git ls-remote -t $REPO_URL | grep -v -e '{}\|alpha\|beta' | cut -d/ -f3 | sort -V | tail -n1)
+VER=$(echo $TAG | sed 's/v//')
+DOWN_URL="https://tukaani.org/xz/xz-$VER.tar.gz"
+FOLDER="$PKG_NAME*"
+VERFILE=""
+INSTALLED_VERSION=$(xz -V | head -n1 | cut -d' ' -f4)
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -L $DOWN_URL | tar xz && cd $FOLDER
+  ./configure --prefix=$HOME/.local --disable-debug && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
