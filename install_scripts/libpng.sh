7afbd3a71d4d39f8c729c9154cdb10034dfbed56
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-25 23:14:26 +0900

    Add libpng install script

---
+++ install_scripts/libpng.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+#    libpng installer
+#
+#    Copyright (C) 2017 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="libpng"
+TMP_DIR=$ROOT/tmp
+REPO_URL="https://github.com/glennrp/libpng"
+TAG=$(git ls-remote -t $REPO_URL | grep -v '{}\|alpha\|beta' | cut -d/ -f3 | sort -V | tail -n1)
+VER=$(echo $TAG | sed 's/v//')
+FOLDER="$PKG_NAME*"
+VERFILE=""
+if $(pkg-config --exists libpng);then
+  INSTALLED_VERSION=$(pkg-config --modversion libpng)
+fi
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -LO $REPO_URL/archive/$TAG.zip
+  unzip -q $TAG.zip && rm -rf $TAG.zip && cd $FOLDER
+  LDFLAGS="-L$HOME/.local/lib" ./autogen.sh && \
+    ./configure --prefix=$HOME/.local && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
