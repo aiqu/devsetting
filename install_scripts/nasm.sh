162b076695d5a3ce6d7cb7be2e4af757e552f37d
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2017-11-25 23:13:56 +0900

    Add nasm install script

---
+++ install_scripts/nasm.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+#    nasm installer
+#
+#    Copyright (C) 2017 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="nasm"
+TMP_DIR=$ROOT/tmp
+REPO_URL="http://www.nasm.us/pub/nasm/releasebuilds/2.13.01/nasm-2.13.01.tar.bz2"
+TAG='2.13.01'
+VER=$TAG
+FOLDER="$PKG_NAME*"
+VERFILE=""
+INSTALLED_VERSION=$(nasm --version | cut -d' ' -f3)
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  echo "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -L $REPO_URL | tar xj && cd $FOLDER
+  ./autogen.sh && \
+    ./configure --prefix=$HOME/.local && \
+    make -j$(nproc) && make install
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  echo "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
