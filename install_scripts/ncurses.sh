e83c544c524ac3d6788042fea66bcb410d704ea5
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-01-06 20:12:56 +0900

    Refactor ncurses to use PKG_NAME and colored prompt

--- install_scripts/ncurses.sh
+++ install_scripts/ncurses.sh
@@ -24,14 +24,17 @@ set -e
 ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 . $ROOT/envset.sh
 
+PKG_NAME="ncurses"
 TMP_DIR=$ROOT/tmp
 REPO_URL=http://invisible-island.net/datafiles/release/ncurses.tar.gz
 FOLDER='ncurses'
 VERFILE="$HOME/.local/include/ncursesw/curses.h"
 if [ -z $REINSTALL ] && [ -r $VERFILE ];then
   INSTALLED_VERSION=$(cat $VERFILE | grep -e 'define NCURSES_VERSION ' | cut -d'"' -f2)
-  echo "ncurses $INSTALLED_VERSION is already installed"
+  gecho "$PKG_NAME $INSTALLED_VERSION is already installed"
 else
+  iecho "$PKG_NAME installation.. pwd: $PWD, root: $ROOT"
+
   mkdir -p $TMP_DIR && cd $TMP_DIR
   curl -L $REPO_URL | tar xz && cd ncurses*
   ./configure --prefix=$HOME/.local --enable-widec --without-develop --without-cxx-binding --with-shared CPPFLAGS='-P' && \
