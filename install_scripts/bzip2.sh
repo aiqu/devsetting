b360202ce8cf3cf7e7fe54eb177f363c7191abea
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-01-08 15:09:13 +0900

    Check existance of bzip2 in local directory

--- install_scripts/bzip2.sh
+++ install_scripts/bzip2.sh
@@ -31,7 +31,7 @@ TAG='1.0.6'
 VER=$TAG
 REPO_URL="http://www.bzip.org/$TAG/bzip2-$TAG.tar.gz"
 FOLDER="$PKG_NAME*"
-INSTALLED_VERSION=$(bzip2 -h 2>&1 | head -n1 | cut -d' ' -f8 | sed 's/,//')
+INSTALLED_VERSION=$($HOME/.local/bin/bzip2 -h 2>&1 | head -n1 | cut -d' ' -f8 | sed 's/,//')
 
 if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
   iecho "$PKG_NAME $VER installation.. pwd: $PWD, root: $ROOT"
