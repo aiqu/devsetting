b2b5534539f25972022fe564509f304b60352799
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-02-08 17:48:34 +0900

    Add ninja install script

---
+++ install_scripts/ninja.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+#    <ninja> installer
+#
+#    Copyright (C) 2018 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+FILENAME=`basename ${BASH_SOURCE[0]}`
+FILENAME=${FILENAME%%.*}
+DONENAME="DONE$FILENAME"
+if [ ! -z ${!DONENAME+x} ];then
+  return 0
+fi
+let DONE$FILENAME=1
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="ninja"
+REPO_URL="https://github.com/ninja-build/ninja"
+TAG=$(git ls-remote -t $REPO_URL | grep -v {} | cut -d/ -f3 | sort -V | tail -n1)
+VER=$(echo $TAG | sed 's/v//')
+FOLDER="$PKG_NAME*"
+VERFILE=""
+$(test -x $LOCAL_DIR/bin/ninja) && INSTALLED_VERSION=$(ninja --version)
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  iecho "$PKG_NAME $VER installation.. install location: $LOCAL_DIR"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -LO $REPO_URL/archive/$TAG.zip
+  unzip -q $TAG.zip && rm -rf $TAG.zip && cd $FOLDER
+  ./configure.py --bootstrap
+  mv ninja $LOCAL_DIR/bin
+else
+  gecho "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
