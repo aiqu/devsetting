5b53c026cc8ba0b4ba63d67d0a6f83c8cf1b9847
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-03-02 12:39:19 +0900

    Add g2o library

---
+++ install_scripts/g2o.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+#    <g2o> installer
+#
+#    Copyright (C) 2018 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+FILENAME=`basename ${BASH_SOURCE[0]}`
+FILENAME=${FILENAME%%.*}
+DONENAME="DONE$FILENAME"
+if [ ! -z ${!DONENAME+x} ];then
+  return 0
+fi
+let DONE$FILENAME=1
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="g2o"
+REPO_URL="https://github.com/RainerKuemmerle/g2o"
+TAG="master"
+VER="master"
+FOLDER="$PKG_NAME*"
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || [ $VER != $INSTALLED_VERSION ]; then
+  iecho "$PKG_NAME $VER installation.. install location: $LOCAL_DIR"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -LO $REPO_URL/archive/$TAG.zip
+  unzip -q $TAG.zip && rm -rf $TAG.zip && cd $FOLDER
+  mkdir -p build && cd build
+  cmake -DCMAKE_INSTALL_PREFIX=${LOCAL_DIR} -DCMAKE_CXX_FLAGS="-O2 -pipe" -DCMAKE_C_FLAGS="-O2 -pipe" ..
+  make -s -j$(nproc)
+  make -s install 1>/dev/null
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  gecho "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
