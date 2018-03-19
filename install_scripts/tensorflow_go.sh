c8730431b230518f1e3efe536827ecef51318ceb
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-03-19 11:03:39 +0900

    Add tensorflow for go

---
+++ install_scripts/tensorflow_go.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+#    tensorflow for go installer
+#
+#    Copyright (C) 2018 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+FILENAME=`basename ${BASH_SOURCE[0]}`
+FILENAME=${FILENAME%%.*}
+DONENAME="DONE$FILENAME"
+if [ ! -z ${!DONENAME+x} ];then
+  return 0
+fi
+let DONE$FILENAME=1
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+. $ROOT/install_scripts/go.sh
+. $ROOT/install_scripts/bazel.sh
+
+TARGET_DIRECTORY=${LOCAL_DIR}
+
+go get -d github.com/tensorflow/tensorflow/tensorflow/go
+cd ${GOPATH}/src/github.com/tensorflow/tensorflow
+./configure
+bazel --build --config opt //tensorflow:libtensorflow.so
+ln -s ${GOPATH}/src/github.com/tensorflow/tensorflow/bazel-bin/tensorflow/libtensorflow.so ${LOCAL_DIR}/lib
+ln -s ${GOPATH}/src/github.com/tensorflow/tensorflow/bazel-bin/tensorflow/libtensorflow_framework.so ${LOCAL_DIR}/lib
+go test github.com/tensorflow/tensorflow/tensorflow/go
+
+cd $ROOT
