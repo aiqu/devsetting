e9f0df4334050e02abe34ab4e5a774cdcec16ea0
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-07-09 03:59:22 +0900

    Add tbb install script

---
+++ install_scripts/tbb.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+#
+#    TBB (Thread building block) installer
+#
+#    Copyright (C) 2018 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+FILENAME=`basename ${BASH_SOURCE[0]}`
+FILENAME=${FILENAME%%.*}
+DONENAME="DONE$FILENAME"
+if [ ! -z ${!DONENAME+x} ];then
+  return 0
+fi
+let DONE$FILENAME=1
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+_R=$REINSTALL
+unset REINSTALL
+REINSTALL=$_R
+unset _R
+
+PKG_NAME="tbb"
+REPO_URL="https://github.com/01org/tbb"
+TAG="2018_U5"
+VER="2018.0.10005"
+FOLDER="$PKG_NAME*"
+VERFILE="${LOCAL_DIR}/include/tbb/tbb_stddef.h"
+INSTALLED_VERSION=
+if [ -r $VERFILE ];then
+  MAJOR=$(cat $VERFILE | grep -m1 TBB_VERSION_MAJOR | cut -d' ' -f3)
+  MINOR=$(cat $VERFILE | grep -m1 TBB_VERSION_MINOR | cut -d' ' -f3)
+  INTERFACE=$(cat $VERFILE | grep -m1 TBB_INTERFACE_VERSION | cut -d' ' -f3)
+  INSTALLED_VERSION="${MAJOR}.${MINOR}.${INTERFACE}"
+fi
+
+if [ ! -z $REINSTALL ] || [ -z $INSTALLED_VERSION ] || $(compare_version $INSTALLED_VERSION $VER); then
+  iecho "$PKG_NAME $VER installation.. install location: $LOCAL_DIR"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -L $REPO_URL/archive/$TAG.tar.gz | tar xz
+  cd $FOLDER
+  python build/build.py --prefix=${LOCAL_DIR} --install-libs --install-devel
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  gecho "$PKG_NAME $VER is already installed"
+fi
+
+cd $ROOT
