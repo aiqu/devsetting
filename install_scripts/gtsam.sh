2e8b46ccacee0429f9703c20455b231bb88cdee4
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-07-16 17:11:56 +0900

    Add gtsam install script

---
+++ install_scripts/gtsam.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+#
+#    <gtsam> installer
+#
+#    Copyright (C) 2018 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+FILENAME=`basename ${BASH_SOURCE[0]}`
+FILENAME=${FILENAME%%.*}
+DONENAME="DONE$FILENAME"
+if [ ! -z ${!DONENAME+x} ];then
+  return 0
+fi
+let DONE$FILENAME=1
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+. $ROOT/install_scripts/cmake.sh
+. $ROOT/install_scripts/hdf5.sh
+. $ROOT/install_scripts/boost.sh
+. $ROOT/install_scripts/tbb.sh
+
+PKG_NAME="gtsam"
+REPO_URL="https://bitbucket.org/gtborg/gtsam"
+TAG=$(git ls-remote -t $REPO_URL | grep -v -e 'rc\|pre\|{}' | cut -d/ -f3 | sort -V | tail -n1)
+CUSTOMTAGNAME="${PKG_NAME}TAG"
+TAG=${!CUSTOMTAGNAME:-$TAG}
+VER=$(echo $TAG | sed 's/-alpha.*//')
+FOLDER="gtborg*"
+VERFILE="$LOCAL_DIR/include/gtsam/config.h"
+INSTALLED_VERSION=
+if [ -r $VERFILE ];then
+  INSTALLED_VERSION=$(grep 'GTSAM_VERSION_STRING "' $VERFILE | cut -d'"' -f2)
+fi
+
+if ([ ! -z $REINSTALL ] && [ $LEVEL -le $REINSTALL ]) || [ -z $INSTALLED_VERSION ] || $(compare_version $INSTALLED_VERSION $VER); then
+  iecho "$PKG_NAME $VER installation.. install location: $LOCAL_DIR"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl -L $REPO_URL/get/$TAG.gz | tar xz
+  cd $FOLDER
+  mkdir -p build && cd build
+  cmake -DCMAKE_BUILD_TYPE=RELEASE \
+    -DCMAKE_INSTALL_PREFIX=${LOCAL_DIR} \
+    -DGTSAM_BUILD_TESTS=OFF \
+    -DGTSAM_BUILD_DOCS=OFF \
+    -DGTSAM_BUILD_EXAMPLES_ALWAYS=OFF \
+    -DGTSAM_BUILD_WRAP=OFF \
+    ..
+  make -s -j${NPROC}
+  make -s install 1>/dev/null
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  gecho "$PKG_NAME $VER is already installed"
+fi
+
+LEVEL=$(( ${LEVEL}-1 ))
+cd $ROOT
