4dbea1c4fe773e49a6b8b0bfeb5b5fc28cd96233
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-08-03 17:27:04 +0900

    Add golang dependency to tensorflow

--- install_scripts/tensorflow.sh
+++ install_scripts/tensorflow.sh
@@ -36,13 +36,11 @@ TF_SRC_DIR=${GOPATH}/src/tensorflow/tensorflow
 TF_PKG_DIR=${HOME}/.lib/tensorflow_pkg
 
 #install prerequisites
-. $ROOT/install_scripts/python.sh
-pip2 install numpy scipy
-pip3 install numpy scipy
 . $ROOT/install_scripts/jdk8.sh
-VER=0.10.1
+. $ROOT/install_scripts/golang.sh
+. $ROOT/install_scripts/python.sh
+pip3 install numpy scipy
 . $ROOT/install_scripts/bazel.sh
-unset VER
 
 #install tensorflow
 go get -d github.com/tensorflow/tensorflow/tensorflow/go
