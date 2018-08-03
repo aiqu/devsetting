c51b598d854523ff4d8a4185513300fae4f15d78
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-08-03 17:25:35 +0900

    Update jdk

--- install_scripts/jdk8.sh
+++ install_scripts/jdk8.sh
@@ -33,18 +33,21 @@ ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
 . $ROOT/envset.sh
 
 PKG_NAME="jdk"
-VER="8u152"
+VER="8u181"
 WORKDIR=${LOCAL_DIR}
 
 if ! java -version 2>&1 | grep -q '1.8' || ! javac -version 2>&1 | grep -q '1.8' || ! which jar 2>/dev/null ;then
   iecho "$PKG_NAME $VER installation.. install location: $LOCAL_DIR"
   mkdir -p $WORKDIR && cd $WORKDIR
 
-  FILENAME='jdk-8u152-linux-x64.tar.gz'
-  FOLDERNAME='jdk1.8.0_152'
+  FILENAME='jdk-8u181-linux-x64.tar.gz'
+  FOLDERNAME='jdk1.8.0_181'
 
-  wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/$FILENAME"
-  tar xf $FILENAME && rm $FILENAME
+  curl -jkL \
+    -H "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+    "http://download.oracle.com/otn-pub/java/jdk/8u181-b13/96a7b8442fe848ef90c96a2fad6ed6d1/$FILENAME" \
+    | tar xz
+  mkdir -p ${LOCAL_DIR}/bin
   ln -s ${LOCAL_DIR}/$FOLDERNAME/bin/* ${LOCAL_DIR}/bin/
 else
   gecho "$PKG_NAME $VER is already installed"
