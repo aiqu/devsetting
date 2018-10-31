220089f0d971689622f6939456db6492dfb07c8c
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-10-31 14:05:43 +0900

    Add a neovim install script

---
+++ install_scripts/neovim.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+#
+#    <neovim> installer
+#
+#    Copyright (C) 2018 Gwangmin Lee
+#    
+#    Author: Gwangmin Lee <gwangmin0123@gmail.com>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+FILENAME=`basename ${BASH_SOURCE[0]}`
+FILENAME=${FILENAME%%.*}
+DONENAME="DONE$FILENAME"
+if [ ! -z ${!DONENAME+x} ];then
+  return 0
+fi
+let DONE$FILENAME=1
+
+ROOT=$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)
+PWD=$(pwd)
+. $ROOT/envset.sh
+
+PKG_NAME="neovim"
+REPO_URL="http://github.com/neovim/neovim"
+TAG=$(git ls-remote -t $REPO_URL | grep -v {} | cut -d/ -f3 | sort -V | tail -n1)
+CUSTOMTAGNAME="$(echo ${PKG_NAME} | sed 's/-//')TAG"
+TAG=${!CUSTOMTAGNAME:-$TAG}
+VER=$(echo $TAG | sed 's/v//')
+FOLDER="$PKG_NAME*"
+INSTALLED_VERSION=$(nvim --version | head -n1 | cut -d' ' -f2 | sed 's/v//')
+
+if ([ ! -z $REINSTALL ] && [ $LEVEL -le $REINSTALL ]) || [ -z $INSTALLED_VERSION ] || $(compare_version $INSTALLED_VERSION $VER); then
+  iecho "$PKG_NAME $VER installation.. install location: $LOCAL_DIR"
+
+  mkdir -p $TMP_DIR && cd $TMP_DIR
+  curl --retry 10 -L $REPO_URL/archive/$TAG.tar.gz | tar xz && cd $FOLDER
+  make -s -j${NPROC} CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$LOCAL_DIR -DCMAKE_BUILD_TYPE=RelWithDebInfo"
+  make -s install 1>/dev/null
+
+  pip install -U neovim
+
+  cd $ROOT && rm -rf $TMP_DIR
+else
+  gecho "$PKG_NAME $VER is already installed"
+fi
+
+LEVEL=$(( ${LEVEL}-1 ))
+cd $ROOT
