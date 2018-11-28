de1eedeec3949ac9fc55a1a7945d2a5aba970354
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-11-28 18:35:02 +0900

    Fix bugs on fish configuration

--- configurations/functions/setunion.fish
+++ configurations/functions/setunion.fish
@@ -4,12 +4,28 @@ function setunion -S -d "add values of second variable not exist on first variab
     return
   end
   if not set -q $argv[1]
-    set -xl $argv[1] $$argv[2]
+    set -x $argv[1] $$argv[2]
     return
   end
   for p in $$argv[2]
     if not string match -q $p $$argv[1]
       set $argv[1] $p $$argv[1]
+    end
+  end
+end
+
+function setunion2 -S -d "add values of second variable not exist on first variable to first variable and form a single string"
+  if [ (count $argv) -ne 2 ]
+    echo "setunion: exact two arguments expected"
+    return
+  end
+  if not set -q $argv[1]
+    set -x $argv[1] (string join ":" $$argv[2])
+    return
+  end
+  for p in $$argv[2]
+    if not string match -q $p $$argv[1]
+      set $argv[1] $p":"$$argv[1]
     end
   end
 end
