fe7835276e98b629f157629bd57631ff7ca0121c
Gwangmin Lee <gwangmin.lee@naverlabs.com>
2018-12-06 12:13:40 +0900

    Fix set union to deal with substring matches

--- configurations/functions/setunion.fish
+++ configurations/functions/setunion.fish
@@ -23,8 +23,19 @@ function setunion2 -S -d "add values of second variable not exist on first varia
     set -x $argv[1] (string join ":" $$argv[2])
     return
   end
+  set -l vals (string split ':' $$argv[1])
   for p in $$argv[2]
-    if not string match -q "*$p*" $$argv[1]
+    set -l matched 0
+    for v in $vals
+      if not string length -q $v
+        continue
+      end
+      if string match -q $p $v
+        set matched 1
+        break
+      end
+    end
+    if test $matched -eq 0
       set $argv[1] $p":"$$argv[1]
     end
   end
